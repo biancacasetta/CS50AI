bb77b69283cc6f7b3240d96553384fb576e6f482
Bianca Casetta <biancacasetta@gmail.com>
2024-08-03 04:28:32 +0200

    heredity done

--- Project 2/heredity/heredity.py
+++ Project 2/heredity/heredity.py
@@ -139,8 +139,41 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    
-    
+    joint_probability = 1.00
+
+    for p, data in people.items():
+        gene_quantity = {p in one_gene: 1, p in two_genes: 2}.get(True, 0)
+        
+        if not data["mother"] and not data["father"]:
+            joint_probability *= PROBS["gene"][gene_quantity]
+        else:
+            mother, father = data["mother"], data["father"]
+            m_genes = {mother in one_gene: 1, mother in two_genes: 2}.get(True, 0)
+            f_genes = {father in one_gene: 1, father in two_genes: 2}.get(True, 0)
+            joint_probability *= calculate_heritage(gene_quantity, m_genes, f_genes)
+
+        has_trait = True if p in have_trait else False
+        joint_probability *= PROBS["trait"][gene_quantity][has_trait]
+
+    return joint_probability
+
+def calculate_heritage(person_genes, mother_genes, father_genes):
+
+    passing_probability = {0: PROBS["mutation"], 1: 0.5, 2: 1 - PROBS["mutation"]}
+    match person_genes:
+        case 0:
+            heritage_probability = ((1 - passing_probability[mother_genes])
+                                    * (1 - passing_probability[father_genes]))
+        case 1:
+            heritage_probability = (passing_probability[mother_genes]
+                                    * (1 - passing_probability[father_genes])
+                                    + (1 - passing_probability[mother_genes])
+                                    * passing_probability[father_genes])
+        case 2:
+            heritage_probability = (passing_probability[mother_genes]
+                                    * passing_probability[father_genes])
+
+    return heritage_probability
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
@@ -150,7 +183,11 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for name in probabilities:
+        gene_quantity = {name in one_gene: 1, name in two_genes: 2}.get(True, 0)
+        has_trait = True if name in have_trait else False
+        probabilities[name]["gene"][gene_quantity] += p
+        probabilities[name]["trait"][has_trait] += p
 
 
 def normalize(probabilities):
@@ -158,7 +195,13 @@ def normalize(probabilities):
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    for probs in probabilities.values():
+        gene_sum = sum(probs["gene"].values())
+        trait_sum = sum(probs["trait"].values())
+        for g in probs["gene"]:
+            probs["gene"][g] /= gene_sum
+        for t in probs["trait"]:
+            probs["trait"][t] /= trait_sum
 
 
 if __name__ == "__main__":
